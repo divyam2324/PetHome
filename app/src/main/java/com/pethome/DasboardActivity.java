8ade32fedcb894694baaf46b4ea66df7806e39ce
divyam2324 <divyam2324@gmail.com>
2026-02-11 22:45:53 +0530

    signup page

---
+++ app/src/main/java/com/pethome/DasboardActivity.java
@@ -0,0 +1,14 @@
+package com.pethome;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DasboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dasboard);
+    }
+}
